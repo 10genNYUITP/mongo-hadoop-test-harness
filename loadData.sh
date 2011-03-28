ce91ba28d66975c583b7b4a2d89dd1967f8218a3
Priya Kanth Manda <priyakanth024@gmail.com>
2011-03-27 22:57:33 -0400

    First version of Test Harness

---
+++ loadData.sh
@@ -0,0 +1,17 @@
+/home/r_omio/mongodb-linux-i686-1.8.0/bin/mongo 127.0.0.1:20000/admin setup-shards.js
+ 
+if ! [ -f big.txt ] ; then
+#NOTE wgt should have an e after the w and before the gt (wiki filtering problem apparently)
+#following two lines is to warn you if you run the script w/o fixing it
+#  echo fix the wgt in the script remove this line and rerun!
+#  exit 1
+ 
+  wget http://norvig.com/big.txt
+fi
+ 
+echo loading...
+perl -e 'while (<>) {  s/\"/\\"/g ; print }' < big.txt | sed 's/.*/  db.lines.insert({"line": "&"});/' > big.js
+echo loaded
+ 
+#load data into database
+/home/r_omio/mongodb-linux-i686-1.8.0/bin/mongo 127.0.0.1:20000 big.js
