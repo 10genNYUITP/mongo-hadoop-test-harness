e669448e3dc0f7e2073c4a8a2f582234db7793c3
Priya Kanth Manda <priyakanth024@gmail.com>
2011-04-28 00:11:36 -0400

    a complete working testharness

---
+++ set-env.sh
@@ -0,0 +1,7 @@
+#customize this for your environment
+
+
+
+export PATH=$HOME/mongodb-src:$PATH
+
+
---
+++ src/testharness/GenerateXML.java
@@ -0,0 +1,81 @@
+import java.io.*;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+
+import org.w3c.dom.*;
+
+public class GenerateXML {
+
+	public void generate() {
+		
+		int i = 0;
+
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+			
+			Element root = doc.createElement("TestHarness");
+			doc.appendChild(root);
+			for(ResultStorage nodeVals : ResultStorage.objs) {
+				Element node = doc.createElement("Test");
+				
+				root.appendChild(node);
+					
+					Element id = doc.createElement("ID");
+					id.appendChild(doc.createTextNode(Integer.toString(++i)));
+					node.appendChild(id);
+				
+					Element name = doc.createElement("Name");
+					name.appendChild(doc.createTextNode(nodeVals.name));
+					node.appendChild(name);
+				
+					Element args = doc.createElement("Arguments");
+					StringBuilder sb = new StringBuilder();
+					for(String arg : nodeVals.args) {
+						sb.append(arg + "; ");
+					} args.appendChild(doc.createTextNode(sb.toString()));
+					node.appendChild(args);
+					
+					Element performace = doc.createElement("Performace");
+					performace.appendChild(doc.createTextNode(nodeVals.performance.toString()));
+					node.appendChild(performace);
+					
+					Element md5 = doc.createElement("MD5");
+					md5.appendChild(doc.createTextNode(nodeVals.md5));
+					node.appendChild(md5);
+					
+					Element uS = doc.createElement("Use-Shards");
+					uS.appendChild(doc.createTextNode(nodeVals.shardsCondition));
+					node.appendChild(uS);
+					
+					Element uC = doc.createElement("Use-Chunks");
+					uC.appendChild(doc.createTextNode(nodeVals.chunksCondition));
+					node.appendChild(uC);
+					
+					Element sO = doc.createElement("Slave-OK");
+					sO.appendChild(doc.createTextNode(nodeVals.slaveOK));
+					node.appendChild(sO);
+			}
+			
+			TransformerFactory tf = TransformerFactory.newInstance();
+			Transformer t = tf.newTransformer();
+			DOMSource src = new DOMSource(doc);
+			StreamResult result =  new StreamResult(new File("/home/r_omio/mongo-hadoop-test-harness/mongoresults.xml"));
+			t.transform(src, result);
+			
+		} catch(ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch(FactoryConfigurationError fce) {
+			fce.printStackTrace();
+		} catch (TransformerConfigurationException tce) {
+			tce.printStackTrace();
+		} catch (TransformerException te) {
+			te.printStackTrace();
+		}
+	}
+
+}
