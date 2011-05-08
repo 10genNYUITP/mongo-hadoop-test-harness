0d5e5baa3be8675207578a3e0079fd8e320c85bf
Joseph Shraibman <jks@iname.com>
2011-05-08 17:39:54 -0400

    Catch Throwable instead of Exception

--- src/testharness/ConfigFileReader.java
+++ src/testharness/ConfigFileReader.java
@@ -119,10 +119,11 @@ class ConfigFileReader extends org.xml.sax.ext.DefaultHandler2 {
 				TestCase tc = new TestCase((Tool) o, (String[]) args.toArray(new String[args.size()]));
 				testcases.add(tc);
 				current_testcase = tc;
-			} catch (Exception ex) {
+			} catch (Throwable ex) {
 				ex.printStackTrace();
 				System.err.println("Could not instantiate '"+testName+"', caught "+ex.getClass().getName()+" "+ex.getMessage());
                                 System.err.println("my classpath is: "+System.getProperty("java.class.path"));
+                                System.exit(1);
 			}
 		} 
 		else if ("resultsdb".equals(qName)) {
